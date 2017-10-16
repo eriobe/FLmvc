ded58af21465631a531b4147bc3d661fb2029e55
eriobe <erik.oberg@miun.se>
2017-10-16 10:29:23 +0200

    Första föreläsningen

---
+++ FLmvc/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using FLmvc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FLmvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        private PersonOperations po = new PersonOperations();
+        // GET: Person
+        public ActionResult Index()
+        {
+            var model = po.GetPersons();  
+            return View(model);
+        }
+
+        public ActionResult ShowPerson()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ FLmvc/Data/PersonOperations.cs
@@ -0,0 +1,47 @@
+﻿using FLmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FLmvc.Data
+{
+    public class PersonOperations
+    {
+
+        private List<Person> persons = new List<Person>();
+        
+        public Person GetPerson()
+        {
+            Person p = new Person()
+            {
+                Firstname = "Erik",
+                Lastname = "Öberg",
+                Age = 42,
+                Id = 1
+            };
+            return p;
+        }
+
+        public List<Person> GetPersons()
+        {
+            Person p = new Person()
+            {
+                Firstname = "Erik",
+                Lastname = "Öberg",
+                Age = 42,
+                Id = 1
+            };
+            persons.Add(p);
+            p = new Person()
+            {
+                Firstname = "Erasdfik",
+                Lastname = "asdf",
+                Age = 42,
+                Id = 2
+            };
+            persons.Add(p);
+            return persons;
+        }
+    }
+}
